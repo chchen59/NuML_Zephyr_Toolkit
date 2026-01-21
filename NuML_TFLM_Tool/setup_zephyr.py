5141a2c38798c7ae0b2f179f6cb321989f7d8c82
chchen59 <chchen59@nuvoton.com>
2026-01-21 11:35:32 +0800

    Added setup zephyr script

--- NuML_TFLM_Tool/setup_zephyr.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import sys
-
-# Install the 'west' package using pip
-subprocess.check_call([sys.executable, "-m", "pip", "install", "west"])
-
---
+++ setup_zephyr.py
@@ -0,0 +1,55 @@
+import subprocess
+import sys
+import os
+
+# Install the 'west' package using pip
+command = [sys.executable, "-m", "pip", "install", "--upgrade", "pip"]
+ret = subprocess.run(command)
+if ret.returncode != 0:
+    sys.exit(ret.returncode)
+
+# Initialize a workspace and retrieve repositories
+command = ["west", "init", "zephyrproject"]
+ret = subprocess.run(command)
+if ret.returncode != 0:
+    print("Failed to initialize west workspace and retcode =", ret.returncode)
+    sys.exit(ret.returncode)
+
+# Change directory to the workspace
+os.chdir("zephyrproject")
+
+# Update the workspace to fetch all repositories
+command = ["west", "update"]
+ret = subprocess.run(command)
+if ret.returncode != 0:
+    print("Failed to update west workspace and retcode =", ret.returncode)
+    sys.exit(ret.returncode)
+
+# Export a Zephyr CMake package
+command = ["west", "zephyr-export"]
+ret = subprocess.run(command)
+if ret.returncode != 0:
+    print("Failed to export Zephyr package and retcode =", ret.returncode)
+    sys.exit(ret.returncode)
+
+# Change directory to the workspace
+os.chdir("zephyrproject")
+
+# Install the Python dependencies
+command = ["cmd", "/c", "zephyr\\scripts\\utils\\west-packages-pip-install.cmd"]
+
+ret = subprocess.run(command)
+if ret.returncode != 0:
+    print("Failed to install Zephyr Python dependencies and retcode =", ret.returncode)
+    sys.exit(ret.returncode)
+
+# Install zephyr sdk(toolchain)
+command = ["west", "sdk", "install", "--version", "0.17.4", "-t", "arm-zephyr-eabi"]
+ret = subprocess.run(command)
+if ret.returncode != 0:
+    print("Failed to install Zephyr SDK and retcode =", ret.returncode)
+    sys.exit(ret.returncode)
+
+# Return to original directory
+os.chdir("../")
+print("Setup completed successfully.")
